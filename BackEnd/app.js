999cc7871b35936a30f5e1f631e8e96a5460bc63
Indipa Karannagoda <indipakarannagoda11@gmail.com>
2025-03-20 20:07:20 +0530

    app

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -7,6 +7,9 @@ const app = express();
 //Middleware
 app.use(express.json());
 app.use("/students", router );
+app.use(cors()); 
+app.use("/exams",router); 
+app.use("/users",userrouter);
 
 
 mongoose.connect("mongodb+srv://AcademicAdmin:UsHzE0AhhEcPuH5f@clusteracademic.4hese.mongodb.net/")
