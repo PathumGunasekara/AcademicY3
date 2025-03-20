24a2161ff86662b0436871f7be755610a885e26e
Lasindu-Rasanka <lasindu5909@gmail.com>
2025-03-20 21:23:45 +0530

    Update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -4,18 +4,26 @@ const cors = require("cors");  // ✅ Import cors
 
 const studentRouter = require("./Routes/StudentRoutes");
 const instructorRouter = require("./Routes/InstructerRoutes");
+<<<<<<< Updated upstream
 const examRouter=require("./Routes/ExamRoutes");
+=======
+>>>>>>> Stashed changes
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(cors());  // ✅ No more error
+<<<<<<< Updated upstream
 
 app.use("/students", studentRouter);
 app.use("/instructors", instructorRouter);
 app.use("/exams", examRouter);
+=======
+>>>>>>> Stashed changes
 
+app.use("/students", studentRouter);
+app.use("/instructors", instructorRouter);
 
 mongoose.connect("mongodb+srv://AcademicAdmin:UsHzE0AhhEcPuH5f@clusteracademic.4hese.mongodb.net/")
   .then(() => console.log("Connected to MongoDB"))
