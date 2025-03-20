5ea0923a3e8efac1cfa7718b030163cf90b6a81f
Lasindu-Rasanka <lasindu5909@gmail.com>
2025-03-20 21:22:26 +0530

    update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const router = require("./Routes/StudentRoutes");
+const cors = require("cors");  // ✅ Import cors
 
+const studentRouter = require("./Routes/StudentRoutes");
+const instructorRouter = require("./Routes/InstructerRoutes");
+const examRouter=require("./Routes/ExamRoutes");
 
 const app = express();
 
-//Middleware
+// Middleware
 app.use(express.json());
-app.use("/students", router );
-app.use(cors()); 
-app.use("/exams",router); 
+app.use(cors());  // ✅ No more error
 
+app.use("/students", studentRouter);
+app.use("/instructors", instructorRouter);
+app.use("/exams", examRouter);
 
 
 mongoose.connect("mongodb+srv://AcademicAdmin:UsHzE0AhhEcPuH5f@clusteracademic.4hese.mongodb.net/")
-.then(() => console.log("Connect to MongoDB"))
-.then(() => {
-    app.listen(5000);
-})
-
-.catch((err) => console.log((err)));
+  .then(() => console.log("Connected to MongoDB"))
+  .then(() => {
+    app.listen(5000, () => {
+      console.log("Server is running on port 5000");
+    });
+  })
+  .catch((err) => console.log(err));
