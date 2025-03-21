e954effb980474eeff5326525db5822904241fbc
milni_methsani <milnimethshani123@gmail.com>
2025-03-21 20:02:46 +0530

    Errors fix in app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,41 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
-<<<<<<< HEAD
-=======
-const cors = require("cors"); 
- 
-const studentRouter = require("./Routes/StudentRoutes");
-const instructorRouter = require("./Routes/InstructerRoutes"); 
-const examRouter=require("./Routes/ExamRoutes");
->>>>>>> origin/Development
+const cors = require("cors");
 
+const studentRouter = require("./Routes/StudentRoutes");
+const instructorRouter = require("./Routes/InstructorRoutes"); 
+const examRouter = require("./Routes/ExamRoutes");
 
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());  // ✅ No more error
+app.use(cors());
 
 app.use("/students", studentRouter);
 app.use("/instructors", instructorRouter);
 app.use("/exams", examRouter);
 
-
-
 mongoose.connect("mongodb+srv://AcademicAdmin:UsHzE0AhhEcPuH5f@clusteracademic.4hese.mongodb.net/")
-<<<<<<< HEAD
-.then(() => console.log("Connect to MongoDB"))
-.then(() => {
-    app.listen(5000);
-})
-
-.catch((err) => console.log((err)));
-=======
-  .then(() => console.log("Connected to MongoDB"))
   .then(() => {
+    console.log("Connected to MongoDB");
     app.listen(5000, () => {
-      console.log("Server are running on port 5000");
+      console.log("Server is running on port 5000");
     });
   })
-  .catch((err) => console.log(err)); 
->>>>>>> origin/Development
+  .catch((err) => console.log("Error connecting to MongoDB:", err));
